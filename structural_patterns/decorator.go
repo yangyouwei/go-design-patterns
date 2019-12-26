973044b96fdcd7496eb19a76c9e6d54e5b1d72a6
dabai <yang_youwei81@163.com>
2019-12-26 14:53:57 +0800

    add  comment

--- structural_patterns/decorator.go
+++ structural_patterns/decorator.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
+//定义一个类型。  为decorate的一个参数类型。
 type Object func(string) string
 
 func Decorate(fn Object) Object {
+    //返回一个函数。可以继续被装饰。返回的函数执行结果就是decorate的执行结果。
     return func(base string) string {
 
         ret := fn(base)
