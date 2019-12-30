aa5a49d8a7b0877944cc70fbd10665013377dc57
dabai <yang_youwei81@163.com>
2019-12-30 09:17:43 +0800

    add comment

--- behavioral_patterns/observer.go
+++ behavioral_patterns/observer.go
@@ -38,6 +38,7 @@ type ConcreteSubject struct {
 }
 
 func (cs *ConcreteSubject) Regist(ob Observer) {
+	//struct{}{}  空结构  struct{} 类型 ，｛｝ 实例化
 	cs.Observers[ob] = struct{}{}
 }
 
@@ -52,18 +53,22 @@ func (cs *ConcreteSubject) Notify(e *Event) {
 }
 
 func main() {
+	//创建观察者 容器
 	cs := &ConcreteSubject{
+		//这里的空结构无意义，程序中没有调用。
 		Observers: make(map[Observer]struct{}),
 	}
 
+	//创建观察者
 	cobserver1 := &ConcreteObserver{1}
 	cobserver2 := &ConcreteObserver{2}
-
+	//注册观察者到容器
 	cs.Regist(cobserver1)
 	cs.Regist(cobserver2)
 
 	for i := 0; i < 5; i++ {
 		e := &Event{fmt.Sprintf("msg [%d]", i)}
+		//notify 调用了 e的update方法
 		cs.Notify(e)
 
 		time.Sleep(time.Duration(1) * time.Second)
